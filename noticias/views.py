72e68aa499e88a7eb8e9ca1102a9de6d18c301c7
Ignacio Leon <ignacioandresleon@hotmail.com>
2017-06-23 01:30:55 -0400

    Detalles y Proyecto final

--- noticias/views.py
+++ noticias/views.py
@@ -2,13 +2,13 @@
 from django.shortcuts import render, redirect
 from noticias.models import Category, News
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django.http import Http404
 
 def home(request):
-
-	lista_noticias = News.objects.order_by('-fecha_creacion')
-	
 	noticia_destacada = News.objects.filter(destacada=True).latest('fecha_creacion')
+	
+	lista_noticias = News.objects.filter(~Q(id_noticia = noticia_destacada.id_noticia)).order_by('-fecha_creacion')
 	
 	return render(request, 'base/index.html', {'lista_noticias':lista_noticias, 'destacada': noticia_destacada})
 
